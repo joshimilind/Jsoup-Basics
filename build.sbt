1673975899b2d8aeb68322f322bc9b6cca190885
Milind <milind.joshi@synerzip.com>
2017-11-29 20:21:52 +0530

    body as whole body tag

--- build.sbt
+++ build.sbt
@@ -4,6 +4,4 @@ version := "0.1"
 
 scalaVersion := "2.12.4"
 
-
-
 libraryDependencies += "org.jsoup" % "jsoup" % "1.7.2"
--- src/main/scala/jsoup/examples/jsoup.scala
+++ src/main/scala/jsoup/examples/jsoup.scala
@@ -11,16 +11,25 @@ object jsoup extends App {
     .get()
   val title = doc.title()
   val links = doc.select("a[href]")
-  var cnt = 0
-  val pw = new PrintWriter(new File(("Crawled Links.txt")))
 
+  val body = doc.body().text()
+  var cnt = 1
+  val pw = new PrintWriter(new File(("Crawled Links.txt")))
+//  println("\nbody : " + body)
   for (link <- links) {
-    pw.write("\n\ntitle : " + link.text + "\nlink : " + link.attr("href"))
-    println("\ntitle : " + link.text)
-    println("link : " + link.attr("href"))
+    pw.write(s"\n\n ${cnt})title : " + link.text + "\nlink : " + link.attr("href"))
+    println(s"\n \t ${cnt}) title : " + link.text)
+    println("\t     link : " + link.attr("href"))
+//    println("\nbody : " + body)
     cnt += 1
   }
+
   pw.close()
+
+  val fileBodyPage = new PrintWriter(new File("body of page.txt"))
+  fileBodyPage.write("\n $body")
+  println("\n\n________")
+  println(s"\nBody of page is :\n $body")
   println(s"\n--total Links are : $cnt")
 }
 
