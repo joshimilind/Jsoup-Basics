1b188f7767c84b46510800174924c0881ed2de46
Milind <milind.joshi@synerzip.com>
2017-11-29 20:23:18 +0530

    body as whole body tag

--- src/main/scala/jsoup/examples/jsoup.scala
+++ src/main/scala/jsoup/examples/jsoup.scala
@@ -7,7 +7,8 @@ import scala.collection.JavaConversions._
 object jsoup extends App {
 
   val doc = Jsoup
-    .connect("https://developer.lightbend.com/guides/akka-quickstart-scala/#what-hello-world-does")
+    .connect(
+      "https://developer.lightbend.com/guides/akka-quickstart-scala/#what-hello-world-does")
     .get()
   val title = doc.title()
   val links = doc.select("a[href]")
@@ -15,12 +16,13 @@ object jsoup extends App {
   val body = doc.body().text()
   var cnt = 1
   val pw = new PrintWriter(new File(("Crawled Links.txt")))
-//  println("\nbody : " + body)
+
   for (link <- links) {
-    pw.write(s"\n\n ${cnt})title : " + link.text + "\nlink : " + link.attr("href"))
+    pw.write(
+      s"\n\n ${cnt})title : " + link.text + "\nlink : " + link.attr("href"))
     println(s"\n \t ${cnt}) title : " + link.text)
     println("\t     link : " + link.attr("href"))
-//    println("\nbody : " + body)
+
     cnt += 1
   }
 
@@ -32,4 +34,3 @@ object jsoup extends App {
   println(s"\nBody of page is :\n $body")
   println(s"\n--total Links are : $cnt")
 }
-
