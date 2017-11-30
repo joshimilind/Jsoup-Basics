5c70f4fd7a22fc867c60953c9a2c2552a3d0547d
Milind <milind.joshi@synerzip.com>
2017-11-30 14:19:28 +0530

    something went wrong!

--- src/main/scala/jsoup/examples/jsoup.scala
+++ src/main/scala/jsoup/examples/jsoup.scala
@@ -2,35 +2,39 @@ package jsoup.examples
 
 import org.jsoup.Jsoup
 import java.io._
-
 import scala.collection.JavaConversions._
+
 object jsoup extends App {
 
-  val doc = Jsoup
-    .connect(
-      "https://developer.lightbend.com/guides/akka-quickstart-scala/#what-hello-world-does")
-    .get()
-  val title = doc.title()
-  val links = doc.select("a[href]")
+  try {
+    val doc = Jsoup
+      .connect("http://www.synerzip.com/")
+      .get()
+    val title = doc.title()
+    val links = doc.select("a[href]")
 
-  val body = doc.body().text()
-  var cnt = 1
-  val pw = new PrintWriter(new File(("Crawled Links.txt")))
+    val body = doc.body().text()
+    var cnt = 0
+    val pw = new PrintWriter(new File(("Crawled Links.txt")))
 
-  for (link <- links) {
-    pw.write(
-      s"\n\n ${cnt})title : " + link.text + "\nlink : " + link.attr("href"))
-    println(s"\n \t ${cnt}) title : " + link.text)
-    println("\t     link : " + link.attr("href"))
+    for (link <- links) {
+      cnt += 1
+      pw.write(
+        s"\n\n ${cnt})title : " + link.text + "\nlink : " + link.attr("href"))
+      println(s"\n \t ${cnt}) title : " + link.text)
+      println("\t     link : " + link.attr("href"))
+    }
 
-    cnt += 1
+    pw.close()
+
+    val fileBodyPage = new PrintWriter(new File("body of page.txt"))
+    fileBodyPage.write("\n $body")
+    println("\n\n________")
+    println(s"\nBody of page is :\n $body")
+    println(s"\n--total Links are : $cnt")
+  } catch {
+    case _: Throwable =>
+      println(
+        "\n Something went wrong, may be web site has disabled robot.txt!")
   }
-
-  pw.close()
-
-  val fileBodyPage = new PrintWriter(new File("body of page.txt"))
-  fileBodyPage.write("\n $body")
-  println("\n\n________")
-  println(s"\nBody of page is :\n $body")
-  println(s"\n--total Links are : $cnt")
 }
